0019e9eb1a7b8de688f73a984cc4f593d3b494a2
Alexander Valencia <aperez@hellobuild.co>
2022-04-23 23:18:30 -0500

    Add semanticUI, SASS and remove unused files

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Header } from "semantic-ui-react";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header as="h1">Hello World</Header>
+    </div>
+  );
+};
+
+export default App;
